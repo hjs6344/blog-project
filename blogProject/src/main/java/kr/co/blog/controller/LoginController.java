00ffe98ed999da7f0a25a0d16c5bfc292cef1d69
Dajeong Kim <galnotepink@gmail.com>
2017-05-20 17:16:54 +0900

    initialization
    project start!

---
+++ blogProject/src/main/java/kr/co/blog/controller/LoginController.java
@@ -0,0 +1,29 @@
+package kr.co.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.blog.service.LoginService;
+import kr.co.blog.vo.Test;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@Autowired
+	private LoginService loginService;
+
+	@RequestMapping("/login")
+	public @ResponseBody Test test() {
+		
+		System.out.println("Welcome!! Login Controller!!");
+		Test result = new Test();
+		result.setTest("aaaa");
+		result.setTest2(123);
+		return result;
+	}
+
+}
---
+++ blogProject/src/main/java/kr/co/blog/service/impl/LoginServiceImpl.java
@@ -0,0 +1,10 @@
+package kr.co.blog.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.blog.service.LoginService;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+}
